073add8639e01e08d45509dbb4b3a8d91e76817a
Sachinpandit2809 <sachinpandit2485@gmail.com>
2024-02-02 17:07:17 +0530

    message

---
+++ resource/main.js
@@ -0,0 +1,144 @@
+let bg=document.querySelector("body");
+
+let img1 = document.querySelector("#img1");
+    img1.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img1").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+
+    let img2 = document.querySelector("#img2");
+    img2.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img2").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh";
+        
+        
+    });
+    let img3 = document.querySelector("#img3");
+    img3.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img3").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+let img4 = document.querySelector("#img4");
+    img4.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img4").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+
+    let img5 = document.querySelector("#img5");
+    img5.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img5").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img6 = document.querySelector("#img6");
+    img6.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img6").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img7 = document.querySelector("#img7");
+    img7.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img7").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img8 = document.querySelector("#img8");
+    img8.addEventListener("click",()=>{
+       let imgSrc=document.querySelector("#img8").getAttribute('src');
+        bg.style.backgroundImage="url(" + imgSrc + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img9 = document.querySelector("#img9");
+    img9.addEventListener("click",()=>{
+       let img9Src=document.querySelector("#img9").getAttribute('src');
+        bg.style.backgroundImage="url(" + img9Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img10 = document.querySelector("#img10");
+    img10.addEventListener("click",()=>{
+       let img10Src=document.querySelector("#img10").getAttribute('src');
+        bg.style.backgroundImage="url(" + img10Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img11 = document.querySelector("#img11");
+    img11.addEventListener("click",()=>{
+       let img11Src=document.querySelector("#img11").getAttribute('src');
+        bg.style.backgroundImage="url(" + img11Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img12 = document.querySelector("#img12");
+    img12.addEventListener("click",()=>{
+       let img12Src=document.querySelector("#img12").getAttribute('src');
+        bg.style.backgroundImage="url(" + img12Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img13 = document.querySelector("#img13");
+    img13.addEventListener("click",()=>{
+       let img13Src=document.querySelector("#img13").getAttribute('src');
+        bg.style.backgroundImage="url(" + img13Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img14 = document.querySelector("#img14");
+    img14.addEventListener("click",()=>{
+       let img14Src=document.querySelector("#img14").getAttribute('src');
+        bg.style.backgroundImage="url(" + img14Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img15 = document.querySelector("#img15");
+    img15.addEventListener("click",()=>{
+       let img15Src=document.querySelector("#img15").getAttribute('src');
+        bg.style.backgroundImage="url(" + img15Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img16 = document.querySelector("#img16");
+    img16.addEventListener("click",()=>{
+       let img16Src=document.querySelector("#img16").getAttribute('src');
+        bg.style.backgroundImage="url(" + img16Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img17 = document.querySelector("#img17");
+    img17.addEventListener("click",()=>{
+       let img17Src=document.querySelector("#img17").getAttribute('src');
+        bg.style.backgroundImage="url(" + img17Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img18 = document.querySelector("#img18");
+    img18.addEventListener("click",()=>{
+       let img18Src=document.querySelector("#img18").getAttribute('src');
+        bg.style.backgroundImage="url(" + img18Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img19 = document.querySelector("#img19");
+    img19.addEventListener("click",()=>{
+       let img19Src=document.querySelector("#img19").getAttribute('src');
+        bg.style.backgroundImage="url(" + img19Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img20 = document.querySelector("#img20");
+    img20.addEventListener("click",()=>{
+       let img20Src=document.querySelector("#img20").getAttribute('src');
+        bg.style.backgroundImage="url(" + img20Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img21 = document.querySelector("#img21");
+    img21.addEventListener("click",()=>{
+       let img21Src=document.querySelector("#img21").getAttribute('src');
+        bg.style.backgroundImage="url(" + img21Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img22 = document.querySelector("#img22");
+    img22.addEventListener("click",()=>{
+       let img22Src=document.querySelector("#img22").getAttribute('src');
+        bg.style.backgroundImage="url(" + img22Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
+    let img23 = document.querySelector("#img23");
+    img23.addEventListener("click",()=>{
+       let img23Src=document.querySelector("#img23").getAttribute('src');
+        bg.style.backgroundImage="url(" + img23Src + ")";
+        bg.style.backgroundSize="100vw 100vh"
+    });
